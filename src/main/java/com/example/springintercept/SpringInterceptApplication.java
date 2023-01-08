321358db2903acc596fa240860a393b0278a47f0
feilong <442349569@qq.com>
2023-01-08 15:16:37 +0800

    Initial commit

---
+++ src/main/java/com/example/springintercept/SpringInterceptApplication.java
@@ -0,0 +1,13 @@
+package com.example.springintercept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInterceptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInterceptApplication.class, args);
+    }
+
+}
